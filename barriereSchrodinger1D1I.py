e586665422cd8e83dfc410a1a2d953561b878693
Imane Bateoui <e-ibateoui1@etu.cyu.fr>
2026-06-21 11:12:32 +0200

    Refactor comments and clean up code formatting

--- barriereSchrodinger1D1I.py
+++ barriereSchrodinger1D1I.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 
 hbar = 1
 m = 1
+# constantes 
 
 Nx = 400
-nt = 80000 
+nt = 80000
 
 x = np.linspace(-30, 30, Nx)
 t = np.linspace(0, 8, nt)
@@ -16,7 +17,7 @@ dt = t[1] - t[0]
 k0 = 5
 x0 = -20
 
-# Paramètres barrière 
+# Paramètres barrière
 V0 = 13.0
 a_barriere = 1.0
 
@@ -24,29 +25,34 @@ V = np.zeros(Nx)
 for i in range(Nx):
     if 0 <= x[i] <= a_barriere:
         V[i] = V0
+        # on crée potentiel 0 partout, sauf entre x=0 et x=a_barriere où il vaut V0
 
-# Initialisation (Paquet complexe)
+
+
 psi = np.exp(-((x - x0)**2) / 4) * np.exp(1j * k0 * x)
-
+# on initialise le paquet d'ondes gaussien centré en x0, avec une vitesse k0
 
 fig, ax = plt.subplots()
 
 for it in range(nt - 1):
     for ix in range(1, Nx - 1):
-        # 1. Calcul de dérivée seconde
+        # calcul de dérivée seconde
         derivee_seconde = (psi[ix+1] - 2*psi[ix] + psi[ix-1]) / (dx**2)
-        
-        # 2. Schrödinger
+
+        # schrodinger
         energie_cinetique = - (hbar**2 / (2 * m)) * derivee_seconde
         energie_potentielle = V[ix] * psi[ix]
         dpsi_dt = (energie_cinetique + energie_potentielle) / (1j * hbar)
-        
-        # 3. Mise à jour Euler
+       
+
+        # euler
         psi[ix] = psi[ix] + dt * dpsi_dt
+        # on avance Ψ d'un petit pas de temps dt
 
     # Affichage aux instants clés
     if it == 0 or it == nt//4 or it == nt//2 or it == nt-2:
         ax.plot(x, np.abs(psi)**2, label=f"t={it*dt:.2f}")
+        
 
 # Dessin de la barrière
 ax.axvspan(0, a_barriere, color='gray', alpha=0.3, label="Barrière V0=13")
